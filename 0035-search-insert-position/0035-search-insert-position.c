d3d7e397bbc765ebe8af6286c9047cb92f8ef937
Lishin V S <156478673+Ellvius@users.noreply.github.com>
2024-08-18 11:02:21 +0530

    Delete 0035-search-insert-position/0035-search-insert-position.c

--- 0035-search-insert-position/0035-search-insert-position.c
+++
@@ -1,8 +0,0 @@
-int searchInsert(int* nums, int numsSize, int target) {
-    int i = 0;
-    while(i< numsSize && nums[i]<=target){
-        if(nums[i]==target) return i;
-        i++;
-    }
-    return i;
-}
