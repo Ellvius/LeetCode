dcbb258359f596a5019d01ef47277e50cb65a403
Lishin V S <156478673+Ellvius@users.noreply.github.com>
2024-08-27 22:18:34 +0530

    Time: 81 ms (20.81%), Space: 13 MB (54.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> result;
         for(int i = 0; i<nums.size(); i++){
             int num = target - nums[i];
-            for(int j = 0; j<nums.size(); j++){
+            for(int j = i+1; j<nums.size(); j++){
                 if(i == j) continue;
                 if(nums[j] == num){
                     result.push_back(i);
