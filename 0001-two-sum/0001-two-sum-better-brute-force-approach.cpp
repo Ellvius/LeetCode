114164ad7c3030cd959b1649542cd6809b01d7fc
Lishin V S <156478673+Ellvius@users.noreply.github.com>
2024-08-27 22:18:28 +0530

    Time: 81 ms (20.81%), Space: 13 MB (54.08%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum-better-brute-force-approach.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for(int i = 0; i<nums.size(); i++){
+            int num = target - nums[i];
+            for(int j = i+1; j<nums.size(); j++){
+                if(i == j) continue;
+                if(nums[j] == num){
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+};
