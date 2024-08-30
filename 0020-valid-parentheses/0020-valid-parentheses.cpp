b85a55e18f71ded8122957ebbe606a8d76a3e912
Lishin V S <156478673+Ellvius@users.noreply.github.com>
2024-08-30 18:32:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (52.71%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> t;
+        for(int i = 0; i<s.size(); i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                t.push(s[i]);
+            }
+            else {
+                if (t.empty()) {
+                    return false;
+                }
+                if(s[i]==')'&&t.top()!='(' || s[i]=='}'&&t.top()!='{' || s[i]==']'&&t.top()!='[' ){
+                    return false;
+                }
+                t.pop();
+            }
+        }
+        return t.empty();
+    }
+};
