8a1d5180e69c222df6718d4d7e98aef59ee26940
Lishin V S <156478673+Ellvius@users.noreply.github.com>
2024-09-02 19:08:23 +0000

    Time: 8 ms (62.36%), Space: 18.1 MB (74.8%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         vector<int> result;
         for(int i = 0; i<numbers.size(); i++){
-            int search = binarySearch(numbers, target-numbers[i]);
+            int search = binarySearch(numbers, target-numbers[i],i+1);
             if(search != -1){
                 result.push_back(i+1);
                 result.push_back(search+1);
@@ -14,8 +14,8 @@ public:
     }
 
 private:
-    int binarySearch(vector<int>& numbers, int key){
-        int low = 0;
+    int binarySearch(vector<int>& numbers, int key, int start){
+        int low = start;
         int high = numbers.size() -1;
         
         while(low<=high){
