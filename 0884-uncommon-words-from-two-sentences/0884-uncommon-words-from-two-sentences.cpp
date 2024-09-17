ea49ae9b80f4c41668f7e4bcb343d978c2277b42
Lishin V S <156478673+Ellvius@users.noreply.github.com>
2024-09-17 15:33:08 +0530

    Time: 4 ms (40.63%), Space: 8.9 MB (72.35%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -3,24 +3,18 @@ public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         vector<string> ans;
         unordered_set<string> s;
+        unordered_map<string, int> count;
 
         int sz1 = s1.size();
         int sz2 = s2.size();
         int st = 0;
         int i = 0;
         while (i < sz1) {
-            int len = 0;
             while (s1[i] != ' ' && i != sz1) {
-                len++;
                 i++;
             }
-            string w = s1.substr(st, len);
-            auto in = s.find(w);
-            if (in != s.end()) {
-                s.erase(in);
-            } else {
-                s.insert(w);
-            }
+            string w = s1.substr(st, i - st);
+            count[w]++;
             i++;
             st = i;
         }
@@ -28,24 +22,19 @@ public:
         st = 0;
         i = 0;
         while (i < sz2) {
-            int len = 0;
             while (s2[i] != ' ' && i != sz2) {
-                len++;
                 i++;
             }
-            string w = s2.substr(st, len);
-            auto in = s.find(w);
-            if (in != s.end()) {
-                s.erase(in);
-            } else {
-                s.insert(w);
-            }
+            string w = s2.substr(st, i-st);
+            count[w]++;
             i++;
             st = i;
         }
 
-        for (string el : s) {
-            ans.push_back(el);
+        for (const auto &pair : count) {
+            if(pair.second == 1){
+                ans.push_back(pair.first);
+            }
         }
         return ans;
     }
