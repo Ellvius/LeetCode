0c419d7da3788096466c9cf02f23e0bc04798ff4
Lishin V S <156478673+Ellvius@users.noreply.github.com>
2025-06-26 01:05:10 +0530

    Time: 5 ms (19.66%), Space: 8.4 MB (97.42%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,11 +3,11 @@ public:
     int reverse(int x) {
         long int ans = 0;
         int sign = (x<0) ? -1:1;
-        x = abs(x);
-        while(x>0){
-            int dig = x%10;
+        long int y = abs(static_cast<long int>(x));
+        while(y>0){
+            int dig = y%10;
             ans = 10*ans + dig;
-            x = x / 10;
+            y = y / 10;
         }
         if(ans > 2147483647 || ans < -2147483648){
             return 0;
