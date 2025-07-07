4ccd5a07184001066202f955ef769b58ecec9ec5
Lishin V S <156478673+Ellvius@users.noreply.github.com>
2025-07-08 00:12:04 +0530

    Time: 1 ms (60.05%), Space: 8.7 MB (41.83%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number-rev.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+
+        long rev = 0;
+        int n = x;
+
+        while(n > 0){
+            rev = (rev * 10) + (n % 10);
+            n /= 10;
+        }
+
+        return rev == x;
+    }
+};
