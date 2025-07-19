989b90956b62832c1a8eac6d5017bb8a9558ed96
Lishin V S <156478673+Ellvius@users.noreply.github.com>
2025-07-19 14:56:23 +0530

    Time: 0 ms (100%), Space: 24.8 MB (5.72%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes-brute-force.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int> temp;
+
+        for(int num: nums){
+            if(num != 0) temp.push_back(num);
+        }
+
+        for(int i = 0; i < temp.size(); i++){
+            nums[i] = temp[i];
+        }
+
+        for(int i = temp.size(); i < nums.size(); i++){
+            nums[i] = 0;
+        }
+    }
+};
